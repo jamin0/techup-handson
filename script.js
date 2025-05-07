a32a45968160c4da5ca578b928f9883c40d4ee36
jamin0 <11806216+jamin0@users.noreply.github.com>
2025-05-07 01:42:01 +0000

    js fixed the button 	modified:   script.js

--- script.js
+++ script.js
@@ -3,16 +3,11 @@ function showInput() {
 
     var user_input = document.getElementById("user-input").value
 
-    if (user_input == null) {
-        document.getElementById("showInput").textContent = "Please submit something."
+    if (user_input == "") {
+        document.getElementById("showInput").textContent = ("Please submit something.");
     } else {
-        document.getElementById("showInput").textContent = "Thanks for submitting input - you submitted the following text: " + user_input 
+        document.getElementById("showInput").textContent = ("Thanks for submitting input - you submitted the following text: \"" + user_input +"\"");
     }
 }
 
-
-    var entries = ["Today was a good day", "The sky is blue", 
-    "It's raining heavily now"];
-    
-    console.log(entries);
     
